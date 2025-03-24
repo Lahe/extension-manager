f7df7d7c499c200cb185962549a614e5659508b8
Spree <34194182+Lahe@users.noreply.github.com>
2025-03-24 04:08:22 +0200

    Add zod + router stuff

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,3 +1,5 @@
+import { Route as aboutRoute } from '@/routes/about'
+import { Route as rootRoute } from '@/routes/index'
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
@@ -5,10 +7,10 @@ export const Route = createRootRoute({
   component: () => (
     <>
       <div className="flex gap-2 p-2">
-        <Link to="/" className="[&.active]:font-bold">
+        <Link to={rootRoute.to} className="[&.active]:font-bold">
           Home
         </Link>{' '}
-        <Link to="/about" className="[&.active]:font-bold">
+        <Link to={aboutRoute.to} className="[&.active]:font-bold">
           About
         </Link>
       </div>
