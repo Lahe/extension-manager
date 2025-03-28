7fadd71ab40c50f53f73ca4c6051ebc465ebc1cd
Spree <34194182+Lahe@users.noreply.github.com>
2025-03-29 00:51:37 +0200

    Reduce files in root folder

--- lint-staged.config.js
+++
@@ -1,8 +0,0 @@
-import path from 'path'
-
-export default {
-  '*.js': [
-    'eslint --fix',
-    `prettier --write --ignore-unknown --ignore-path ${path.relative(process.cwd(), '.prettieringore')}`,
-  ],
-}
